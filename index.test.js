a0e52614ca3099588a321f2b93d01d136a03cfe4
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-07-07 22:02:45 +0530

    update package.json

--- index.test.js
+++ index.test.js
@@ -1,20 +1,19 @@
 import { describe, it, expect } from "vitest";
 
 describe("Capitalize", () => {
-  it('returns word with first letter capitalized', () => {
+  it("returns word with first letter capitalized", () => {
     expect(capitalize("black")).toBe("Black");
-  })
+  });
 
-  it('returns multiple words with first letter capitalized', () => {
+  it("returns multiple words with first letter capitalized", () => {
     expect(capitalize("magic kingdom")).toBe("Magic kingdom");
-  })
+  });
 
-  it('works with punctuation', () => {
+  it("works with punctuation", () => {
     expect(capitalize("black")).toBe("Black");
-  })
+  });
 
-  it('does not effect already capitalized words', () => {
+  it("does not effect already capitalized words", () => {
     expect(capitalize("BlaCk")).toBe("BlaCk");
-  })
-
-})
+  });
+});
