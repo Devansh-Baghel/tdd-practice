b6698d8ee99005fdead93bfdadbedfa8550c13b8
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-07-07 21:58:06 +0530

    add tests for capitalize function

---
+++ index.test.js
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+
+describe("Capitalize", () => {
+  it('returns word with first letter capitalized', () => {
+    expect(capitalize("black")).toBe("Black");
+  })
+
+  it('returns multiple words with first letter capitalized', () => {
+    expect(capitalize("magic kingdom")).toBe("Magic kingdom");
+  })
+
+  it('works with punctuation', () => {
+    expect(capitalize("black")).toBe("Black");
+  })
+
+  it('does not effect already capitalized words', () => {
+    expect(capitalize("BlaCk")).toBe("BlaCk");
+  })
+
+})
