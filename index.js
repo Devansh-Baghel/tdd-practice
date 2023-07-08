8d93b3920cbb9c6b62c08306d2590a6b662cdf1f
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-07-08 21:08:11 +0530

    update reverseString function

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ function capitalize(string) {
 }
 
 function reverseString(string) {
-  let result = "";
-
-  for (let i = string.length - 1; i >= 0; i--) {
-    result += string[i];
-  }
-
-  return result;
+  return string.split("").reverse().join("");
 }
+
 export { capitalize, reverseString };
