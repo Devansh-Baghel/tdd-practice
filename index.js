4094b95c708b4390616078ef71d425bd5039d4c8
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-07-09 18:30:38 +0530

    add tests for punctuation in caeser cipher tests

--- index.js
+++ index.js
@@ -25,26 +25,30 @@ const calculator = {
 }
 
 function caeserCipher(string, shift) {
-  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const punctuation = ['.', ',', ';', ':', '!', '?', '-', '(', ')', '[', ']', '{', '}', '"', "'", '`', '...', '—', '–', '•', '/', '\\', '|', '@', '#', '$', '%', '^', '&', '*', '+', '=', '<', '>', '~', " "];
   if (shift === 0) {
     return string;
   }
   
   let result = [];
   string = string.split("");
+
   for (let i in string) {
-    const index = alphabet.indexOf(string[i]);
-    let newIndex = index + shift;
-    // console.log(index)
-    if ((index + shift) > 25){
-      newIndex = (index + shift) - 26;
+    if(punctuation.includes(string[i])){
+      result.push(string[i]);
+    } else {
+      const index = alphabet.indexOf(string[i]);
+      let newIndex = index + shift;
+      if (newIndex > 25){
+        newIndex = (index + shift) - 26;
+      }
+      result.push(alphabet[newIndex]);
     }
-    result.push(alphabet[newIndex]);
   }
   // console.log(result)
   return result.join("");
 }
 
-// caeserCipher("yxz", 7)
+// caeserCipher("yxz", 90)
 export { capitalize, reverseString , calculator, caeserCipher};
